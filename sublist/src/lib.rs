42ba213a261609fb5d5a630688aaf4985706ac31
node.dom <dominique.reese444@gmail.com>
2022-06-13 22:58:53 -0400

    Complete submission 1st

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -13,26 +13,36 @@ pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Compariso
         compare_result = Comparison::Sublist;
     } else if !_first_list.is_empty() && _second_list.is_empty() {
         compare_result = Comparison::Superlist
+
     } else if _first_list.eq(_second_list) {
-        compare_result = Comparison::Equal
-    } else if _first_list.len() > _second_list.len() {
-        // Comparison::Superlist
-        let window_length = _second_list.len();
-        while let Some(window) = _first_list.windows(window_length).next() {
-            if window == _second_list {
-                compare_result = Comparison::Superlist;
-                break;
-            }
-        }
+        compare_result = Comparison::Equal;
     } else {
-        let window_length = _first_list.len();
-        while let Some(window) = _second_list.windows(window_length).next() {
-            if window == _first_list {
-                compare_result = Comparison::Sublist;
-                break;
-            }
+        if _first_list.is_sublist_of(_second_list) {
+            compare_result  = Comparison::Sublist
+        } 
+        if _second_list.is_sublist_of(_first_list) {
+            compare_result = Comparison::Superlist
         }
     }
-
     compare_result
+}
+
+impl<T: std::cmp::PartialEq> List<T> for &[T] {
+    fn is_sublist_of(&self,list :&[T]) -> bool {
+        if self.is_empty() {
+            return false
+        }
+
+        let window_length = self.len();
+        for window in list.windows(window_length) {
+            if *self == window {
+                return true
+            }
+        }
+        false
+    }
+}
+
+trait List<T> {
+    fn is_sublist_of(&self,list :&[T]) -> bool;
 }
