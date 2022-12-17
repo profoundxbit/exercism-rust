4e92d08b4634711feb6e6d9a0b8f832f54fe6179
node.dom <dominique.reese444@gmail.com>
2022-12-16 22:57:18 -0500

    Complete rectangle

--- rectangles/src/lib.rs
+++ rectangles/src/lib.rs
@@ -15,23 +15,26 @@ pub fn count(lines: &[&str]) -> u32 {
             // If next line is not valid at corner indexes exit.
             // Repeat for end of prev line corner til next corner.
             //Skip lines with no corner char
-            let corner_pairs = line.match_indices(CORNER_CHAR).combinations(2); 
+            let corner_pairs = line.match_indices(CORNER_CHAR).combinations(2);
             for corner_pair in corner_pairs {
                 dbg!(&corner_pair);
-                for &l in &lines[index+1..] {
+                for &l in &lines[index + 1..] {
                     let index = corner_pair[0].0;
                     let l: Vec<_> = l.chars().collect();
                     let rect_side_one = l[index];
-                    let index = corner_pair[1].0;
-                    let rect_side_two = l[index];
-
+                    let index_two = corner_pair[1].0;
+                    let rect_side_two = l[index_two];
+                    let in_between_chars = &l[index..=index_two];
+                    //dbg!(&in_between_chars);
                     if not_side(rect_side_one) || not_side(rect_side_two) {
                         break;
                     }
 
                     if rect_side_one == CORNER_CHAR && rect_side_two == CORNER_CHAR {
-                        println!("Rectangle found!");
-                        rectangles_count += 1;
+                        if !in_between_chars.contains(&' ') {
+                            println!("Rectangle found!");
+                            rectangles_count += 1;
+                        }
                     }
                 }
             }
